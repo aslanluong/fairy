66dc585b81daa8e62ecefb90fafcd05bd4a37ce5
Aslan <luongththang@gmail.com>
2021-04-12 23:17:33 +0700

    feat: add embed flag for say command

--- internal/commands/cmd_say.go
+++ internal/commands/cmd_say.go
@@ -3,7 +3,10 @@ package commands
 import (
 	"errors"
 	"flag"
+	"fmt"
 	"strings"
+
+	"github.com/bwmarrin/discordgo"
 )
 
 type CmdSay struct {
@@ -26,17 +29,46 @@ func (c *CmdSay) Exec(ctx *Context) (err error) {
 
 	f := flag.NewFlagSet("sayflags", flag.ContinueOnError)
 	fHide := f.Bool("hide", false, "hide original message")
+	fEmbed := f.Bool("embed", false, "send message with embed")
 	if err = f.Parse(args); err != nil {
 		return errors.New("invalid flag(s) provided")
 	}
 
 	if !*fHide {
+		if *fEmbed {
+			_, err = ctx.Session.ChannelMessageSendEmbed(ctx.Message.ChannelID,
+				&discordgo.MessageEmbed{Title: fmt.Sprintf("%s said:", ctx.Message.Author), Description: strings.Join(f.Args(), " ")})
+			return
+		}
 		_, err = ctx.Session.ChannelMessageSend(ctx.Message.ChannelID, strings.Join(f.Args(), " "))
 		return
 	}
 
-	if err = ctx.Session.ChannelMessageDelete(ctx.Message.ChannelID, ctx.Message.ID); err == nil {
-		_, err = ctx.Session.ChannelMessageSend(ctx.Message.ChannelID, strings.Join(f.Args(), " "))
+	errs := make(chan error, 1)
+
+	if *fEmbed {
+		go func() {
+			_, err = ctx.Session.ChannelMessageSendEmbed(ctx.Message.ChannelID,
+				&discordgo.MessageEmbed{Title: fmt.Sprintf("%s said:", ctx.Message.Author), Description: strings.Join(f.Args(), " ")})
+
+			errs <- err
+		}()
+		go func() {
+			err = ctx.Session.ChannelMessageDelete(ctx.Message.ChannelID, ctx.Message.ID)
+			errs <- err
+		}()
+		<-errs
+		return
 	}
+
+	go func() {
+		_, err = ctx.Session.ChannelMessageSend(ctx.Message.ChannelID, strings.Join(f.Args(), " "))
+		errs <- err
+	}()
+	go func() {
+		err = ctx.Session.ChannelMessageDelete(ctx.Message.ChannelID, ctx.Message.ID)
+		errs <- err
+	}()
+	<-errs
 	return
 }
